5ea36dd86e01f5cda8f6981b7d0bde0a6f8c4164
Andrei Kvapil <kvapss@gmail.com>
2024-12-09 19:47:38 +0100

    bump Talos Linux image for cozystack
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- pkg/generated/presets.go
+++ pkg/generated/presets.go
@@ -126,7 +126,7 @@ cluster:
 	"cozystack/values.yaml": `endpoint: "https://192.168.100.10:6443"
 clusterDomain: cozy.local
 floatingIP: 192.168.100.10
-image: "ghcr.io/aenix-io/cozystack/talos:v1.8.2"
+image: "ghcr.io/aenix-io/cozystack/talos:v1.8.3"
 podSubnets:
 - 10.244.0.0/16
 serviceSubnets:

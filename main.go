3b7635ccc56b7c8457b06067bbd62afc92022bd6
Andrei Kvapil <kvapss@gmail.com>
2025-01-10 16:56:27 +0100

    Fix ALPN connection issue
    Ref: https://github.com/siderolabs/talos/issues/9463
    
    Signed-off-by: Andrei Kvapil <kvapss@gmail.com>

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"gopkg.in/yaml.v3"
 
 	"github.com/aenix-io/talm/pkg/commands"
+	_ "github.com/siderolabs/talos/cmd/talosctl/acompat"
 	"github.com/siderolabs/talos/cmd/talosctl/cmd/common"
 	"github.com/siderolabs/talos/pkg/machinery/constants"
 	"github.com/spf13/cobra"
